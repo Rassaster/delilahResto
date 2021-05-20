632bbb8172ebc058a9310b3da91bef92fe09c82a
JuanRassa <jmrassaproduccion@gmail.com>
2021-05-20 08:42:57 -0500

    Updating successful message content when connecting to DB.

--- backEnd/dataBase/dbConnect.js
+++ backEnd/dataBase/dbConnect.js
@@ -8,7 +8,7 @@ const sequelize = new Sequelize(`${DIALECT_DB}://${USER_DB}:${PASS_DB}@${HOST_DB
 // Connecting to the Data Base and logging in console the success/failure of the process:
 sequelize.authenticate()
   .then(()=>{
-    console.log("Te has conectado con la Base de Datos:");
+    console.log(`Te has conectado con la Base de Datos: ${NAME_DB}.`);
   })
   .catch(err => {
     console.log("Ha habido un error de conexión con la Base de Datos:", err);
