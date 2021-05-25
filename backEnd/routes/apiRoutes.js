98cce06e0c5fb534d3de40c09cc5250512aedc2a
JuanRassa <jmrassaproduccion@gmail.com>
2021-05-24 22:54:40 -0500

    Creating /register route.

--- backEnd/routes/apiRoutes.js
+++ backEnd/routes/apiRoutes.js
@@ -1,5 +1,7 @@
 // Requiring "Router" object from "Express":
 const router = require("express").Router();
+const apiRegister = require("./api/register");
 
+router.use("/register", apiRegister)
 // Exports:
 module.exports = router;
