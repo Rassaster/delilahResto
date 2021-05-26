199c969348ba334835853792150dc4ddd41be16d
JuanRassa <jmrassaproduccion@gmail.com>
2021-05-25 20:17:51 -0500

    Changing language to English.

--- backEnd/sql/queries.js
+++ backEnd/sql/queries.js
@@ -1,13 +1,23 @@
 // Requiring Data Base connection's module from dbConnect.js:
 const sequelize = require("../dataBase/dbConnect");
-// SQL INSERT Queries:
-const newRegister =  (usuario, nombre_apellido, email, numero_celular, direccion, clave, salt, es_admin) => {
-  return sequelize.query("INSERT INTO usuarios(usuario, nombre_apellido, email, numero_celular, direccion, clave, salt,  es_admin) VALUES(?, ?, ?, ?, ?, SHA2(?, 224), ?, ?)", {
-    replacements: [usuario, nombre_apellido, email, numero_celular, direccion, clave, salt, es_admin],
+// ***** SQL INSERT QUERIES *****
+// INSERT create new user in Users:
+const newUser =  (username, fullname, email, cellphone_number, delivery_address, user_password, salt, is_admin) => {
+  return sequelize.query("INSERT INTO users(username, fullname, email, cellphone_number, delivery_address, user_password, salt,  is_admin) VALUES(?, ?, ?, ?, ?, SHA2(?, 224), ?, ?)", {
+    replacements: [username, fullname, email, cellphone_number, delivery_address, user_password, salt, is_admin],
     type: sequelize.QueryTypes.INSERT
+  });
+};
+// ***** SQL INSERT QUERIES *****
+// SELECT * FROM usuarios WHERE usuario = ?
+const findUser = (searchedUser) => {
+  return sequelize.query("SELECT * FROM users WHERE user = ?", {
+    replacements: [searchedUser],
+    type: sequelize.QueryTypes.SELECT
   });
 };
 // Exports:
 module.exports = {
-  newRegister
+  newUser,
+  findUser
 }
