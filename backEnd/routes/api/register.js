9bad4ee36854d6f350c628cb7d4ac70d612d790c
JuanRassa <jmrassaproduccion@gmail.com>
2021-05-25 23:56:31 -0500

    Updating endpoint with hashPassword() middleware.

--- backEnd/routes/api/register.js
+++ backEnd/routes/api/register.js
@@ -1,16 +1,23 @@
 // Requiring "Router" object from "Express":
 const router = require("express").Router();
+const { hashPassword } = require("../../middlewares/users");
 // Requiring SQL-Sequelize queries commands:
 const { newUser } = require("../../sql/queries");
 // ******************** ENDPOINTS ******************** //
 // -> /delilahResto/register/register (either as User or Admin):
-router.post("/register", (req, res) => {
-  const {username, fullname, email, cellphone_number, delivery_address, user_password, salt, is_admin} = req.body;
-  newUser(username, fullname, email, cellphone_number, delivery_address, user_password, salt, is_admin)
+router.post("/register", hashPassword, (req, res) => {
+  const {hashedPasswordHex, uuidSalt} = req.derivedKey;
+  const {username, fullname, email, cellphone_number, delivery_address, is_admin} = req.body;
+  newUser(username, fullname, email, cellphone_number, delivery_address, hashedPasswordHex, uuidSalt, is_admin)
     .then(result => {
       let successResponse = {
         SuccessMessage: "User created successfully.",
-        CreatedUser: req.body,
+        CreatedUser: {
+          username: req.body.username,
+          email: req.body.email,
+          cellphone_number: req.body.cellphone_number,
+          is_admin: req.body.is_admin
+        },
         id_CreatedUser: result[0]
       };
       res.status(201).json(successResponse);
