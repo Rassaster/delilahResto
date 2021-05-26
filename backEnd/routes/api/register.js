db56f465c3a3e871e24beeba1807c6797caa1319
JuanRassa <jmrassaproduccion@gmail.com>
2021-05-25 20:21:32 -0500

    Changing language to English.

--- backEnd/routes/api/register.js
+++ backEnd/routes/api/register.js
@@ -1,25 +1,25 @@
 // Requiring "Router" object from "Express":
 const router = require("express").Router();
 // Requiring SQL-Sequelize queries commands:
-const { newRegister } = require("../../sql/queries");
+const { newUser } = require("../../sql/queries");
 // ******************** ENDPOINTS ******************** //
-// -> /delilahResto/register/newUser (either as User or Admin):
-router.post("/newUser", (req, res) => {
-  const {usuario, nombre_apellido, email, numero_celular, direccion, clave, salt, es_admin} = req.body;
-  newRegister(usuario, nombre_apellido, email, numero_celular, direccion, clave, salt, es_admin)
+// -> /delilahResto/register/register (either as User or Admin):
+router.post("/register", (req, res) => {
+  const {username, fullname, email, cellphone_number, delivery_address, user_password, salt, is_admin} = req.body;
+  newUser(username, fullname, email, cellphone_number, delivery_address, user_password, salt, is_admin)
     .then(result => {
       let successResponse = {
-        SuccessDescription: "El usuario se ha creado con éxito",
-        UsuarioCreado: req.body,
-        idUsuarioCreado: result[0]
+        SuccessDescription: "User created successfully:",
+        CreatedUser: req.body,
+        id_CreatedUser: result[0]
       };
       res.status(201).json(successResponse);
     })
     .catch(err => {
       let errorResponse = {
         ErrorMessage: err.parent.sqlMessage,
-        ErrorDescription: "Revisar documentación sobre cómo se debe estructurar el formato JSON en relación a la Columna indicada en ErrorMessage.",
-        InformacionEnviada: req.body
+        ErrorDescription: "Please review the API Documentation in relation to the JSON format expected.",
+        ReceivedJSON: req.body
       };
       res.status(400).send(errorResponse); 
     })
