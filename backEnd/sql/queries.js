a81a3b5ff32120fdf847246f73de2a241ec769b2
JuanRassa <jmrassaproduccion@gmail.com>
2021-05-25 23:55:08 -0500

    Adjusting newUser() and findUser() SQL Syntax.

--- backEnd/sql/queries.js
+++ backEnd/sql/queries.js
@@ -3,7 +3,7 @@ const sequelize = require("../dataBase/dbConnect");
 // ***** SQL INSERT QUERIES *****
 // INSERT create new user in Users:
 const newUser =  (username, fullname, email, cellphone_number, delivery_address, user_password, salt, is_admin) => {
-  return sequelize.query("INSERT INTO users(username, fullname, email, cellphone_number, delivery_address, user_password, salt,  is_admin) VALUES(?, ?, ?, ?, ?, SHA2(?, 224), ?, ?)", {
+  return sequelize.query("INSERT INTO users(username, fullname, email, cellphone_number, delivery_address, user_password, salt, is_admin) VALUES(?, ?, ?, ?, ?, ?, ?, ?)", {
     replacements: [username, fullname, email, cellphone_number, delivery_address, user_password, salt, is_admin],
     type: sequelize.QueryTypes.INSERT
   });
@@ -11,7 +11,7 @@ const newUser =  (username, fullname, email, cellphone_number, delivery_address,
 // ***** SQL INSERT QUERIES *****
 // SELECT * FROM usuarios WHERE usuario = ?
 const findUser = (searchedUser) => {
-  return sequelize.query("SELECT * FROM users WHERE user = ?", {
+  return sequelize.query("SELECT * FROM users WHERE username = ?", {
     replacements: [searchedUser],
     type: sequelize.QueryTypes.SELECT
   });
