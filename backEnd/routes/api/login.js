f4e3e18b7cc1c9bf7c96b3778eec3f27e4247bb1
JuanRassa <jmrassaproduccion@gmail.com>
2021-05-30 22:45:26 -0500

    Added userExistanceCheck() as a middleware to '/login' endpoint.

--- backEnd/routes/api/login.js
+++ backEnd/routes/api/login.js
@@ -1,39 +1,10 @@
 // Requiring "Router" object from "Express":
 const router = require("express").Router();
-const { findUser } = require("../../sql/queries");
-// Middlewares
-const userExist =  async (req, res, next) => {
-  const { username } = req.body;
-  const user = await findUser(username);
-  if (user.length === 0) {
-    let message = `The user "${username}" doesn't exist.`
-    res.status(404).send(message);
-  } else {
-    next();
-  }
-}
-const getUserInfo = async (req, res, next) => {
-  const { username } = req.body;
-  const user = await findUser(username);
-  req.userInfo = user;
-  next();
-}
-
-
+const { userExistanceCheck } = require("../../middlewares/users")
 // -> /delilahResto/login (either as User or Admin):
-router.post("/login", userExist, getUserInfo, (req, res) => {
-  // const { username, user_password } = req.body;
+router.post("/login", userExistanceCheck, (req, res) => {
   console.log(req.userInfo);
   res.json(req.userInfo);
 })
-
-
-
-
-
-
-
-
-
 // Exports:
 module.exports = router;
