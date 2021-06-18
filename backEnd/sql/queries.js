fc7926514e0f3d300b08854fe949b07f4ca84042
JuanRassa <jmrassaproduccion@gmail.com>
2021-06-18 09:46:15 -0500

    Simplifying SELECT queries to two functions.

--- backEnd/sql/queries.js
+++ backEnd/sql/queries.js
@@ -9,64 +9,23 @@ const newUser =  (username, fullname, email, cellphone_number, delivery_address,
   });
 };
 // ***** SQL SELECT QUERIES *****
-// SELECT * FROM usuarios WHERE usuario = ?
-const selectUserByEmail = (searchedUserEmail) => {
-  return sequelize.query("SELECT * FROM users WHERE email = ?", {
-    replacements: [searchedUserEmail],
+// SELECT * FROM ("table");
+const selectAllFromTable = (table) => {
+  return sequelize.query(`SELECT * FROM ${table}`, {
+    type: sequelize.QueryTypes.SELECT
+  });
+}
+// SELECT * FROM ("table") WHERE ("field") = ?;
+const selectFromTableWhereFieldIsValue = (table, field, value) => {
+  return sequelize.query(`SELECT * FROM ${table} WHERE ${field} = ?`, {
+    replacements: [value],
     type: sequelize.QueryTypes.SELECT
   });
 };
-const selectUserByUsername = (searchedUserUsername) => {
-  return sequelize.query("SELECT * FROM users WHERE username = ?", {
-    replacements: [searchedUserUsername],
-    type: sequelize.QueryTypes.SELECT
-  });
-};
-const selectAllUsers = () => {
-  return sequelize.query("SELECT * FROM users", {type: sequelize.QueryTypes.SELECT});
-};
-const selectUserById = (userId) => {
-  return sequelize.query("SELECT * FROM users WHERE id_user = ?", {
-    replacements: [userId],
-    type: sequelize.QueryTypes.SELECT
-  });
-};
-const selectAllProducts = () => {
-  return sequelize.query("SELECT * FROM products", {type: sequelize.QueryTypes.SELECT});
-};
-const selectProductByName = (productName) => {
-  return sequelize.query("SELECT * FROM products WHERE product_name = ?", {
-    replacements: [productName],
-    type: sequelize.QueryTypes.SELECT
-  });
-};
-const selectProductById = (productId) => {
-  return sequelize.query("SELECT * FROM products WHERE id_product = ?", {
-    replacements: [productId],
-    type: sequelize.QueryTypes.SELECT
-  });
-};
-const selectAllOrders = () => {
-  return sequelize.query("SELECT * FROM orders", {type: sequelize.QueryTypes.SELECT});
-};
-const selectOrderById = (orderId) => {
-  return sequelize.query("SELECT * FROM orders WHERE id_order = ?", {
-    replacements: [orderId],
-    type: sequelize.QueryTypes.SELECT
-  });
-};
-
-
+// 
 // Exports:
 module.exports = {
   newUser,
-  selectUserByEmail,
-  selectUserByUsername,
-  selectAllUsers,
-  selectUserById,
-  selectAllProducts,
-  selectProductByName,
-  selectProductById,
-  selectAllOrders,
-  selectOrderById
+  selectFromTableWhereFieldIsValue,
+  selectAllFromTable,
 }
