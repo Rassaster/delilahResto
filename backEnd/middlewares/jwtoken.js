26df0412ab540f52eac7a7d97e1a955e7b8b78a3
JuanRassa <jmrassaproduccion@gmail.com>
2021-06-18 21:44:41 -0500

    jwtokenExtraction() and jwtokenVerification() middlewares ready.

--- backEnd/middlewares/jwtoken.js
+++ backEnd/middlewares/jwtoken.js
@@ -9,31 +9,46 @@ const jwtokenGenerator = (req, res, next) => {
     delete req.userInfo[0].cellphone_number;
     delete req.userInfo[0].delivery_address;
     const payload = req.userInfo[0];
-    const token = jwt.sign(payload, JWT_SECRET, {expiresIn : "2min"})
+    const token = jwt.sign(payload, JWT_SECRET, {expiresIn : "20min"})
     req.jwtoken = token;
     next()
   } catch {
     const error = new Error();
-    error.name = "Token generation error."
+    error.name = "Token generation error.";
     error.message = "An error has occurred while the jwt was being created.";
     error.status = 500;
     res.send(error);
   }
 }
+const jwtokenExtraction = (req, res, next) => {
+  try {
+    const authorizationHeader = req.headers.authorization.split(" ");
+    const extractedJWT = authorizationHeader[1];
+    req.extractJWT = extractedJWT;
+    next();
+  } catch {
+    const error = new Error();
+    error.name = "Token extraction error.";
+    error.message = "An error has occurred while extracting the JWToken from the headers. Please verify that the authorization token is included in the headers.";
+    error.status = 500;
+    res.send(error);
+  }
+}
 const jwtokenVerification = (req, res, next) => {
-    const token = req.jwtoken
-    const verification = jwt.verify(token, JWT_SECRET, (err, decoded) => {
-      if (err) {
-        res.json(err);
-      }
-      if (decoded) {
-        console.log(decoded)
-        next();
-      }
-    })
+  const jwTokenAccess = req.extractJWT;
+  const verification = jwt.verify(jwTokenAccess, JWT_SECRET, (err, decoded) => {
+    if (err) {
+      res.json(err);
+    }
+    if (decoded) {
+      req.jwtokenDecoded = decoded;
+      next();
+    }
+  })
 }
 
 module.exports = {
   jwtokenGenerator,
+  jwtokenExtraction,
   jwtokenVerification
 }
