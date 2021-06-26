51796a65a729281f675b92f00c333f136b2727ca
JuanRassa <jmrassaproduccion@gmail.com>
2021-06-26 14:59:10 -0500

    Creation of get endpoint for creating new product. Missing to midwares.

--- backEnd/routes/api/products-endpoint.js
+++ backEnd/routes/api/products-endpoint.js
@@ -1,5 +1,12 @@
-const router = require("expres").Router();
-
-
+const router = require("express").Router();
+const { useFunc } = require("ajv/dist/compile/util");
+// Import neccesary middlewares:
+const { createNewProduct } = require("../../middlewares/products-midwares");
+// Create new product:
+router.post("/", createNewProduct, (req, res) => {
+  if (req.productCreation["Status"] === 201) {
+    res.status(201).json(req.productCreation)
+  };
+});
 // Exports:
 module.exports = router;
