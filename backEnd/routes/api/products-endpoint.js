77070c036e0c46a8c0402eb95e250562b6dbb95b
JuanRassa <jmrassaproduccion@gmail.com>
2021-06-27 14:06:58 -0500

    Search product by Id is ready.

--- backEnd/routes/api/products-endpoint.js
+++ backEnd/routes/api/products-endpoint.js
@@ -1,15 +1,25 @@
 const router = require("express").Router();
-const { useFunc } = require("ajv/dist/compile/util");
-// Requiring neccesary middlewares:
+// Schema middlewares:
 const { validateJSONSchema } = require("../../middlewares/JSONvalidation");
-const { createNewProduct } = require("../../middlewares/products-midwares");
+// JWT middlewares:
+const {jwtokenExtraction, jwtokenVerification } = require("../../middlewares/jwtoken");
+// User middlewares
+const { checkAdminCredentials, justAdminGate } = require("../../middlewares/users-midwares");
+// Products middlewares:
+const { createNewProduct, getProductById } = require("../../middlewares/products-midwares");
 // Requiring JSON schemas:
 const { productSchema } = require("../../schema/schemas");
-// Create new product:
-router.post("/", validateJSONSchema(productSchema), createNewProduct, (req, res) => {
+// ******************** ENDPOINTS ******************** //
+// Create new product. Just admin.
+router.post("/", jwtokenExtraction, jwtokenVerification, checkAdminCredentials, justAdminGate, validateJSONSchema(productSchema), createNewProduct, (req, res) => {
   if (req.productCreation["Status"] === 201) {
     res.status(201).json(req.productCreation)
   };
+});
+// Search product by Id. Just admin.
+router.get("/productId:productId", jwtokenExtraction, jwtokenVerification, checkAdminCredentials, justAdminGate, getProductById, (req, res) =>{
+  res.status(200).json(req.productById);
+  delete req.productById["ProductFound"];
 });
 // Exports:
 module.exports = router;
