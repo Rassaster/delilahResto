6116815148d60fa08b5f788d0012e948655eb736
JuanRassa <jmrassaproduccion@gmail.com>
2021-06-27 12:39:35 -0500

    Requiring and adding validateJSONSchema and productSchema into create new product endpoint.

--- backEnd/routes/api/products-endpoint.js
+++ backEnd/routes/api/products-endpoint.js
@@ -1,9 +1,12 @@
 const router = require("express").Router();
 const { useFunc } = require("ajv/dist/compile/util");
-// Import neccesary middlewares:
+// Requiring neccesary middlewares:
+const { validateJSONSchema } = require("../../middlewares/JSONvalidation");
 const { createNewProduct } = require("../../middlewares/products-midwares");
+// Requiring JSON schemas:
+const { productSchema } = require("../../schema/schemas");
 // Create new product:
-router.post("/", createNewProduct, (req, res) => {
+router.post("/", validateJSONSchema(productSchema), createNewProduct, (req, res) => {
   if (req.productCreation["Status"] === 201) {
     res.status(201).json(req.productCreation)
   };
