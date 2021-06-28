65d964b0fef7ada1637be1dc50abb03de48e888e
JuanRassa <jmrassaproduccion@gmail.com>
2021-06-28 15:48:33 -0500

    updateProductById() midware ready.

--- backEnd/middlewares/products-midwares.js
+++ backEnd/middlewares/products-midwares.js
@@ -85,8 +85,38 @@ const getAllProducts = async (req, res, next) => {
   }
 }
 // -updateProductById
-const updateProductById = (req, res, next) => {
-
+const updateProductById = async (req, res, next) => {
+  try {
+    // If product is NOT found, doesn't exist, the operation is stoped:
+    if (!req.productById["ProductFound"]) {
+      okReponse200["Message"] = "Product not found.";
+      okReponse200["Result"] = `The product with id ${req.params.productId} doesn't exist, therefore,there is no information to be updated. Please proceed to the product creation endopoint.`;
+      okReponse200["ProductFound"] = false;
+      req.updateProductByID = okReponse200;
+    }
+    // If the product IS found, the UPDATE query is executed:
+    if (req.productById["ProductFound"]) {
+      // The UPDATE query returns an array. 
+      const product = await updateTableRegisterWhereIdIsValue("products", req.body, "id_product", req.params.productId);
+      // // If array[1] === 0 -> No information was updated.
+      if (product[1] === 0) {
+        conflictResponse409["Message"] = "No information was updated.";
+        conflictResponse409["Result"] = `The information of the product with id ${req.params.productId} did not suffer any changes. The data that was sent matches exactly with the one already registered.`;
+        conflictResponse409["ProductUpdated"] = false;
+        req.updateProductByID = conflictResponse409;
+        // // If array[1] === 1 -> Changes have been received and updated.
+      } else if (product[1] === 1) {
+        okReponse200["Message"] = "Product information updated with success.";
+        okReponse200["Result"] = req.body;
+        okReponse200["ProductUpdated"] = true;
+        req.updateProductByID = okReponse200;
+      }
+    }
+    next();
+  } catch {
+    internalServerError500["Message"] = "An error has occurred while updating the product's information by id.";
+    res.send(internalServerError500);
+  }
 };
 // -deleteProductById
 const deleteProductById = (req, res, next) => {
