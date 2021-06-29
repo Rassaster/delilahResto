834888dfb2e3df3316dfcdf5bea8bf9eca0d4cd2
JuanRassa <jmrassaproduccion@gmail.com>
2021-06-29 08:41:11 -0500

    Writing the base for start working on the firsth and hardest endpoint.

--- backEnd/routes/api/orders-endpoint.js
+++ backEnd/routes/api/orders-endpoint.js
@@ -2,11 +2,18 @@ const router = require("express").Router();
 // Requiring JSON schemas:
 const { orderSchema } = require("../../schema/schemas");
 // ******************** MIDDLEWARES ******************** //
-
+// Schema middlewares:
+const { validateJSONSchema } = require("../../middlewares/JSONvalidation");
+// JWT middlewares:
+const { jwtokenExtraction, jwtokenVerification } = require("../../middlewares/jwtoken");
+// Security/Credentials middlewares:
+const { checkAdminCredentials, justAdminGate } = require("../../middlewares/users-midwares");
+// CRUD middlewares:
+const { createNewOrder } = require("../../middlewares/orders-midwares");
 // ******************** ENDPOINTS ******************** //
 // -> /delilahResto/orders/new. For either Admins or Users:
-router.post("/new", (req, res) => {
-
+router.post("/new", jwtokenExtraction, jwtokenVerification, checkAdminCredentials, validateJSONSchema(orderSchema), (req, res) => {
+  res.status(200).json(req.body)
 });
 // Exports:
 module.exports = router;
