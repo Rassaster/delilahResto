0b9d440d7fbfe8713820ee3093b4ca1682027571
JuanRassa <jmrassaproduccion@gmail.com>
2021-06-29 18:59:06 -0500

    createNeworder() midware. Missing: comments and INSERT to required_products table.

--- backEnd/middlewares/orders-midwares.js
+++ backEnd/middlewares/orders-midwares.js
@@ -1,15 +1,68 @@
 // Import Server Responses:
 const {  okReponse200, createdResponse201, forbiddenResponse401, notAuthorizedResponse403, conflictResponse409, internalServerError500 } = require("../serverResponses");
 // Import MYSQL Queries functions:
-const { newProduct, selectFromTableWhereFieldIsValue, selectAllFromTable, selectProductsJoinCategories, updateTableRegisterWhereIdIsValue, deleteTableRegisterWhereIdIsValue } = require("../sql/queries");
+const { newOrder, selectFromTableWhereFieldIsValue, selectAllFromTable, selectProductsJoinCategories, updateTableRegisterWhereIdIsValue, deleteTableRegisterWhereIdIsValue } = require("../sql/queries");
 // ***************************************** MIDDLEWARES *********************************************
 // -createNewOrder:
 const createNewOrder = (req, res, next) => {
-try {
-  res.status(200).send("YOU ARE IN")
-} catch {
-  
-}
+  try {
+  let producsQuantityStr = "";
+  let totalOrderCost = 0;
+  let orderProductsInfo = {};
+  const productsIds = req.body["products"];
+  const checkExistanceOfProductsById = async () => {
+    let tempProductsArr = [];
+    for (i = 0; i < productsIds.length; i++) {
+      let product = await selectFromTableWhereFieldIsValue("products", "id_product", productsIds[i]);
+      if (product.length === 0) {
+        okReponse200["Message"] = "Product not found.";
+        okReponse200["Result"] = `The product with id ${req.params.productId} doesn't exist.`;
+        okReponse200["ProductFound"] = false;
+        req.productById = okReponse200;
+        next();
+        break;
+      } else {
+        tempProductsArr.push(product[0]["product_name"]);
+        orderProductsInfo[product[0]["product_name"]] = {"product_price" : product[0]["product_price"]}
+      }
+    }
+    while (tempProductsArr.length > 0) {
+      let arr = [];
+      arr = tempProductsArr.filter(product => product === tempProductsArr[0]);
+      orderProductsInfo[tempProductsArr[0]].amount = arr.length;
+      for (j = 0; j < tempProductsArr.length; j++) {
+        if (tempProductsArr[j] === arr[0]) {
+          if ((tempProductsArr.splice(j, 1)).length !== 0) {
+            j--;
+          };
+        }
+      }
+    }
+    return orderProductsInfo;
+  }
+  checkExistanceOfProductsById()
+    .then(async orderProductsInfo => {
+      for (product in orderProductsInfo) {
+        totalOrderCost += orderProductsInfo[product]["product_price"] * orderProductsInfo[product]["amount"];
+        producsQuantityStr += `${orderProductsInfo[product]["amount"]} x ${product}, `
+      }
+      producsQuantityStr = producsQuantityStr.slice(0, -2);
+      let createdOrder = await newOrder(req.jwtokenDecoded.id_user, producsQuantityStr, totalOrderCost, req.body.id_paying_method);
+      let order = {
+        id_order: createdOrder[0],
+        username: req.jwtokenDecoded.username,
+        products: producsQuantityStr,
+        total_cost: totalOrderCost
+      }
+      createdResponse201["Message"] = "Order created successfully.";
+      createdResponse201["Result"] = order;
+      req.createdOrder = createdResponse201;
+      next();
+    })
+    .catch(err => console.log(err));
+  } catch (error) {
+    console.log(error)
+  }
 }
 // Exports:
 module.exports = {
