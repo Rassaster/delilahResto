17d1d603f2802d7ecf3597e7f9a40d78d77e2755
JuanRassa <jmrassaproduccion@gmail.com>
2021-06-29 08:18:07 -0500

    Creating the new route for orders' endpoint.

--- backEnd/routes/apiRoutes.js
+++ backEnd/routes/apiRoutes.js
@@ -2,10 +2,11 @@
 const router = require("express").Router();
 const apiUsers = require("./api/users-endpoint");
 const apiProducts = require("./api/products-endpoint");
+const apiOrders = require("./api/products-endpoint");
 
 router.use("/users", apiUsers);
 router.use("/products", apiProducts);
-// router.use("/orders", apiUser);
-// router.use("/favorites", apiUser);
+router.use("/orders", apiOrders);
+
 // Exports:
 module.exports = router;
