04b0c8c4563f682bd41622f63da781a901383a37
JuanRassa <jmrassaproduccion@gmail.com>
2021-06-30 16:21:01 -0500

    Get order by Id endpoint is ready.

--- backEnd/routes/api/orders-endpoint.js
+++ backEnd/routes/api/orders-endpoint.js
@@ -9,7 +9,7 @@ const { jwtokenExtraction, jwtokenVerification } = require("../../middlewares/jw
 // Security/Credentials middlewares:
 const { checkAdminCredentials, justAdminGate } = require("../../middlewares/users-midwares");
 // CRUD middlewares:
-const { createNewOrder } = require("../../middlewares/orders-midwares");
+const { createNewOrder, getOrderById } = require("../../middlewares/orders-midwares");
 // ******************** ENDPOINTS ******************** //
 // -> /delilahResto/orders/new. For either Admins or Users:
 router.post("/new", jwtokenExtraction, jwtokenVerification, checkAdminCredentials, validateJSONSchema(orderSchema), createNewOrder, (req, res) => {
@@ -19,5 +19,10 @@ router.post("/new", jwtokenExtraction, jwtokenVerification, checkAdminCredential
     res.status(201).json(req.createdOrder)
   }
 });
+// -> /delilahResto/orders/orderId::orderId. Just for Admins:
+router.get("/orderId::orderId", jwtokenExtraction, jwtokenVerification, checkAdminCredentials, justAdminGate, getOrderById, (req, res) => {
+  res.status(200).json(req.orderById);
+  delete req.orderById["OrderFound"];
+})
 // Exports:
 module.exports = router;
