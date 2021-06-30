7a885d106ce65e1178917e41db214b40deb0a66e
JuanRassa <jmrassaproduccion@gmail.com>
2021-06-30 17:19:17 -0500

    /allOrder endpoint is ready.

--- backEnd/routes/api/orders-endpoint.js
+++ backEnd/routes/api/orders-endpoint.js
@@ -9,20 +9,25 @@ const { jwtokenExtraction, jwtokenVerification } = require("../../middlewares/jw
 // Security/Credentials middlewares:
 const { checkAdminCredentials, justAdminGate } = require("../../middlewares/users-midwares");
 // CRUD middlewares:
-const { createNewOrder, getOrderById } = require("../../middlewares/orders-midwares");
+const { createNewOrder, getOrderById, getAllOrders } = require("../../middlewares/orders-midwares");
 // ******************** ENDPOINTS ******************** //
 // -> /delilahResto/orders/new. For either Admins or Users:
 router.post("/new", jwtokenExtraction, jwtokenVerification, checkAdminCredentials, validateJSONSchema(orderSchema), createNewOrder, (req, res) => {
   if (req.createdOrder.Status === 200) {
-    res.status(200).json(req.createdOrder)
+    res.status(200).json(req.createdOrder);
   } else if (req.createdOrder.Status === 201) {
-    res.status(201).json(req.createdOrder)
-  }
+    res.status(201).json(req.createdOrder);
+  };
 });
 // -> /delilahResto/orders/orderId::orderId. Just for Admins:
 router.get("/orderId::orderId", jwtokenExtraction, jwtokenVerification, checkAdminCredentials, justAdminGate, getOrderById, (req, res) => {
   res.status(200).json(req.orderById);
   delete req.orderById["OrderFound"];
-})
+});
+// -> /delilahResto/orders/allOrders. 
+//// Admins can access all system's orders, while Users just to their own orders:
+router.get("/allOrders", jwtokenExtraction, jwtokenVerification, checkAdminCredentials, getAllOrders, (req, res) => {
+  res.status(200).json(req.getAllOrders);
+});
 // Exports:
 module.exports = router;
