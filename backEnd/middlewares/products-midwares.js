50b3371843fa4ab9374a079f43b06f9554fcad06
JuanRassa <jmrassaproduccion@gmail.com>
2021-07-01 22:01:08 -0500

    Deleting unused imports.

--- backEnd/middlewares/products-midwares.js
+++ backEnd/middlewares/products-midwares.js
@@ -1,5 +1,5 @@
 // Import Server Responses:
-const {  okReponse200, createdResponse201, forbiddenResponse401, notAuthorizedResponse403, conflictResponse409, internalServerError500 } = require("../serverResponses")
+const {  okReponse200, createdResponse201, conflictResponse409, internalServerError500 } = require("../serverResponses")
 // Import MYSQL Queries functions:
 const { newProduct, selectFromTableWhereFieldIsValue, selectAllFromTable, selectProductsJoinCategories, updateTableRegisterWhereIdIsValue, deleteTableRegisterWhereIdIsValue } = require("../sql/queries"); 
 // ***************************************** MIDDLEWARES *********************************************
