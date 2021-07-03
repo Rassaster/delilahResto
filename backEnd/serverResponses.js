51f9ba22bbac0c300fc7122eef7b6d4396f7936e
JuanRassa <jmrassaproduccion@gmail.com>
2021-07-03 07:13:01 -0500

    Correcting misunderstanding betweeen status 401 and 403.

--- backEnd/serverResponses.js
+++ backEnd/serverResponses.js
@@ -3,44 +3,56 @@ const okReponse200 = {
   Status: 200,
   Message: "",
   Result: []
-}
+};
 const createdResponse201 = {
   Status: 201,
   Message: "User created successfully.",
   Result : []
-}
+};
 const badRequesResponse400 = {
   Status: 400,
   Message: "",
   Result: ""
-}
-const forbiddenResponse401 = {
+};
+const unauthorizedResponse401 = {
   Status: 401,
-  Message: "",
-  Result: "Forbidden access."
-}
-const notAuthorizedResponse403 = {
-  Status: 403,
   Message: "The user's cretendials doesn't allow them to complete this request. Only an Administrator has the authorization.",
   Result: "Unauthorized."
 };
+// const forbiddenResponse401 = {
+//   Status: 401,
+//   Message: "",
+//   Result: "Forbidden access."
+// }
+const forbiddenResponse403 = {
+  Status: 403,
+  Message: "",
+  Result: "Forbidden access."
+};
+// const notAuthorizedResponse403 = {
+//   Status: 403,
+//   Message: "The user's cretendials doesn't allow them to complete this request. Only an Administrator has the authorization.",
+//   Result: "Unauthorized."
+// };
 const conflictResponse409 = {
   Status: 409,
   Message: " ",
   Result: "Conflict."
-}
+};
 const internalServerError500 = {
   Satus: 500,
   Message: " ",
   Restult: "Internal Server Error."
-}
+};
 // Exports:
 module.exports = {
   okReponse200,
   createdResponse201,
-  forbiddenResponse401,
+  unauthorizedResponse401,
+  // forbiddenResponse401,
   badRequesResponse400,
-  notAuthorizedResponse403,
+  forbiddenResponse403,
+  // notAuthorizedResponse403,
   conflictResponse409,
   internalServerError500
-}
+};
